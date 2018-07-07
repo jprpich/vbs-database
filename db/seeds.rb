fbfa3a01943e5f736f44e23bc5aee51445fb8774
Joshua <joshuaprpich@gmail.com>
2018-07-07 11:15:18 -0500

    delete duplicate Dir[File.join] line

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
-  load seed
-end
-
 Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
   load seed
 end
