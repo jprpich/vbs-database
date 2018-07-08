a52bf9b6fd540fae72c24bbff12c688529205ddc
Joshua <joshuaprpich@gmail.com>
2018-07-08 08:08:51 -0500

    fix tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "visiting the homepage" do |variable|
   scenario "the visitor sees welcome text" do
     visit root_path
-    expect(page).to have_text("Padres VBS Colombia")
+    expect(page).to have_text("You need to sign in or sign up before continuing.")
   end
 end
