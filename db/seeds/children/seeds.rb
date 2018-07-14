7288bebde400e96325e06ce1b0eec6b33eaf0441
Joshua <joshuaprpich@gmail.com>
2018-07-14 15:18:39 -0500

    fix seed files

--- db/seeds/children/seeds.rb
+++ db/seeds/children/seeds.rb
@@ -1,4 +1,12 @@
 50.times do 
-    Child.create({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, age: Faker::Number.number(1), identification: Faker::Number.number(8), parent_id: Faker::Number.between(1, 10) })
-  end
+  Child.create(
+    {
+      first_name: Faker::Name.first_name, 
+      last_name: Faker::Name.last_name, 
+      age: Faker::Number.number(1), 
+      identification: Faker::Number.number(8), 
+      parent_id: Faker::Number.between(1, 10) 
+    }
+  )
+end
     
--- db/seeds/parents/seeds.rb
+++ db/seeds/parents/seeds.rb
@@ -1,13 +1,13 @@
- 10.times do 
+10.times do 
   Parent.create(
-      {
-        first_name: Faker::FunnyName.name, 
-        last_name: Faker::FunnyName.name, 
-        phone: Faker::Number.number(10), 
-        email: Faker::Internet.email, 
-        church: Faker::Team.name, 
-        amount_paid: Faker::Number.number(6)
-      }
-    )
-  end
-    
+    {
+      first_name: Faker::FunnyName.name, 
+      last_name: Faker::FunnyName.name, 
+      phone: Faker::Number.number(10), 
+      email: Faker::Internet.email, 
+      church: Faker::Team.name, 
+      amount_paid: Faker::Number.number(6)
+    }
+  )
+end
+  
