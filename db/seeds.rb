6f04c15ddb4f9c1578308a8d76e53eaedf15f4e9
Joshua <joshuaprpich@gmail.com>
2018-07-14 15:22:44 -0500

    change seed files for production test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
-  load seed
+# Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
+#   load seed
+# end
+
+#  
+
+10.times do 
+  Parent.create(
+    {
+      first_name: Faker::FunnyName.name, 
+      last_name: Faker::FunnyName.name, 
+      phone: Faker::Number.number(10), 
+      email: Faker::Internet.email, 
+      church: Faker::Team.name, 
+      amount_paid: Faker::Number.number(6)
+    }
+  )
 end
 
- 
+50.times do 
+  Child.create(
+    {
+      first_name: Faker::Name.first_name, 
+      last_name: Faker::Name.last_name, 
+      age: Faker::Number.number(1), 
+      identification: Faker::Number.number(8), 
+      parent_id: Faker::Number.between(1, 10) 
+    }
+  )
+end
+    
