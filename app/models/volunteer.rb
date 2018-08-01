61e6a26ca1aaee7a0dc76ed57e660ac862c5043d
Joshua <joshuaprpich@gmail.com>
2018-08-01 10:30:46 -0500

    fix max num

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -5,7 +5,7 @@ class Volunteer < ApplicationRecord
   validates :email, presence: true  
   validates :church, presence: true  
   validates :role_type, presence: true
-  validates :identification, numericality: { less_than_or_equal_to: 2020745719,  only_integer: true }
+  validates :identification, numericality: { less_than_or_equal_to: 2147483647,  only_integer: true }
   enum role_type: { logistics: 0, decoration: 1 }
 
   scope :by_role_type, -> (role_type) { where("role_type = ?", role_type) }
