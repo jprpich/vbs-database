03a0ed6558c65f888e423d5eb9ec4b6e0385f923
Joshua <joshuaprpich@gmail.com>
2018-08-27 12:38:56 -0500

    add get hackers redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :hackers
+  get '/hackers', to: redirect('/hackers/sign_up')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :parents do
     resources :children, only: :create
   end
-  
+
   get 'asign_crews', to: 'children#asign_crews'
 
   resources :children
